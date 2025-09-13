1e70ffe5b465eb80387b7984d930d9182dd154f2
francesco-mauro <francesco.mauro180@gmail.com>
2025-09-13 12:31:11 +0200

    Method HB

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,20 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-//        for (int i = 1; i <= 3; i++){
-//
-//            for (int j = 1; j <= 9; j++) {
-//                System.out.print(j + " ");
-//            }
-//            System.out.println();
-//
-//        }
+        happyBirthday();
+        happyBirthday();
+        happyBirthday();
 
-        Scanner scanner = new Scanner(System.in);
 
-        int rows;
-        int columns;
-        char symbol;
-
-        System.out.println("Enter the numbers of rows: ");
-        rows = scanner.nextInt();
-
-        System.out.println("Enter the numbers of columns: ");
-        columns = scanner.nextInt();
-
-        System.out.println("Enter the symbol to use: ");
-        symbol = scanner.next().charAt(0);
-
-        for (int j = 0; j < rows; j++) {
-            for (int i = 0; i < columns; i++) {
-                System.out.print(symbol);
-            }
-            System.out.println();
-        }
-
-        scanner.close();
 
     }
+    static void happyBirthday(){
+        System.out.println("Happy birthday to you!");
+        System.out.println("Happy birthday to you!");
+        System.out.println("You are X years old!");
+        System.out.println("Happy birthday to you!\n");
+    }
+
+
 }
 
