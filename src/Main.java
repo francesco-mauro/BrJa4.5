3eaa4f90ab23e9c8059428e77911c92bb23e0ac4
francesco-mauro <francesco.mauro180@gmail.com>
2025-09-13 12:58:26 +0200

    Method Fn

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,39 @@
 public class Main {
     public static void main(String[] args) {
 
-        happyBirthday();
-        happyBirthday();
-        happyBirthday();
+        String name = "Bro";
+        int age = 25;
 
 
+        double result = cube(3);
+        System.out.println(result);
+
+        String fullName= getFullName("Spongebob", "Squarepants");
+        System.out.println(fullName);
 
     }
-    static void happyBirthday(){
+
+    static void happyBirthday(String name, int age) {
         System.out.println("Happy birthday to you!");
-        System.out.println("Happy birthday to you!");
-        System.out.println("You are X years old!");
+        System.out.printf("Happy birthday to %s!\n", name);
+        System.out.printf("You are %d years old!\n", age);
         System.out.println("Happy birthday to you!\n");
     }
 
+    static double square(double number) {
+        return number * number;
+    }
+
+    static double cube(double number) {
+        return number * number * number;
+    }
+
+    static String getFullName(String first, String last){
+        return first + " " + last;
+    }
 
 }
+
+
+
 
