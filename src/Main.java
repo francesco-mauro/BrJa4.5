d6bf91a04e85cc5ae93ba58d7d8d5fc72f7dd19b
francesco-mauro <francesco.mauro180@gmail.com>
2025-09-15 20:10:34 +0200

    new modules errata v.1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
 
 
@@ -5,7 +8,19 @@ public class Main {
 
         String[] fruits = {"apple", "orange", "banana", "coconut"};
 
-        System.out.println(fruits[2]);
+        int numOfFruits = fruits.length;
+
+        Arrays.sort(fruits);
+        Arrays.fill(fruits, "pineapple");
+
+        for (int i = 0; i < fruits.length; i++){
+            System.out.println(fruits[i]);
+        }
+
+
+
+        System.out.println(fruits[0]);
+        System.out.println(numOfFruits);
 
 
     }
