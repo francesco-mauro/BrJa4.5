6b04694fd655d16f5c4f06181e7a2d579a036292
francesco-mauro <francesco.mauro180@gmail.com>
2025-09-18 15:41:01 +0200

    Food programming

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
 
 
     public static void main(String[] args) {
 
-        String[] fruits = {"apple", "orange", "banana", "coconut"};
+        Scanner scanner = new Scanner(System.in);
 
+        String[] foods;
+        int size;
+
+        System.out.print("What number of foods o you want? ");
+        size = scanner.nextInt();
+        scanner.nextLine();
+
+        foods = new String[size];
+
+
+        for (int i = 0; i < foods.length; i++) {
+            System.out.println("enter a food: ");
+            foods[i] = scanner.nextLine();
+
+        }
+
+        System.out.println("------------------------");
+
+        for (String food : foods) {
+            System.out.println(food);
+        }
+
+//        System.out.println(foods.length);
+
+        scanner.close();
 
 
     }
