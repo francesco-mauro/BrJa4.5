e497ab0c5c9440c05938d6daa0b2cf94ef618cdf
francesco-mauro <francesco.mauro180@gmail.com>
2025-09-18 16:41:15 +0200

    Packing

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,32 @@
-import java.util.Scanner;
 
 public class Main {
 
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        System.out.println(average(1, 2, 3, 4));
 
-
-        int[] numbers = {1, 9, 2, 8, 3, 5, 4};
-        String[] fruits = {"apple", "orange", "banana"};
-        boolean isFound = false;
-        String target;
-
-        System.out.println("enter a fruit to search for: ");
-        target = scanner.nextLine();
-
-
-
-        for (int i = 0; i < fruits.length; i++) {
-            if (fruits[i].equals(target)) {
-                System.out.println("Element found at index: " + i + "\n" + "Position: " + (i + 1));
-                isFound = true;
-                break;
-            }
-
-        }
-
-        if (!isFound) {
-            System.out.println("Element not found in the array");
-        }
-
-        scanner.close();
     }
 
+//    static int add(int... numbers) {
+//        int sum = 0;
+//
+//        for (int number : numbers) {
+//            sum += number;
+//        }
+//        return sum;
+//
+//    }
+
+    static double average(double... numbers) {
+        double sum = 0;
+
+        for (double number : numbers) {
+            sum += number;
+        }
+        return sum / numbers.length;
+
+    }
 
 }
 
