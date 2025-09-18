faa17e7400f21c13b6c10c0eb2797e6c87ea682b
francesco-mauro <francesco.mauro180@gmail.com>
2025-09-18 16:15:51 +0200

    Scanner implementation

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
 
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+
         int[] numbers = {1, 9, 2, 8, 3, 5, 4};
-        int target = 2;
+        String[] fruits = {"apple", "orange", "banana"};
+        boolean isFound = false;
+        String target;
+
+        System.out.println("enter a fruit to search for: ");
+        target = scanner.nextLine();
 
 
-        for (int i = 0; i < numbers.length; i++) {
-            if (target == numbers[i]) {
-                System.out.println("Element found at index: " + i + "\n" + "position: " + (i + 1));
+
+        for (int i = 0; i < fruits.length; i++) {
+            if (fruits[i].equals(target)) {
+                System.out.println("Element found at index: " + i + "\n" + "Position: " + (i + 1));
+                isFound = true;
                 break;
             }
 
         }
 
+        if (!isFound) {
+            System.out.println("Element not found in the array");
+        }
 
+        scanner.close();
     }
 
 
