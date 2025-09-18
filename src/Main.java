6e603882502bcf5b8106d0090bd2912056bfe21e
francesco-mauro <francesco.mauro180@gmail.com>
2025-09-18 17:44:14 +0200

    new array v2

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String[] fruits = {"apple", "orange", "banana"};
-        String[] vegetables = {"potato", "onion", "carrot"};
-        String[] meats = {"chicken", "pork", "beef", "fish"};
+//        String[] fruits = {"apple", "orange", "banana"};
+//        String[] vegetables = {"potato", "onion", "carrot"};
+//        String[] meats = {"chicken", "pork", "beef", "fish"};
 
-        String[][] groceries = {fruits, vegetables, meats };
+        char[][] telephone = {{'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '8'},
+                {'*', '0', '#'}};
 
-        for (String[] foods : groceries){
-            for (String food : foods){
-                System.out.print(food + " ");
+
+        for (char[] row : telephone) {
+            for (char number : row) {
+                System.out.print(number + " ");
             }
             System.out.println();
         }
