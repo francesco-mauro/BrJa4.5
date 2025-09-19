3e8622873adc90923dd118d78a15167beaec883c
francesco-mauro <francesco.mauro180@gmail.com>
2025-09-19 15:54:31 +0200

    Quiz v1

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,34 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        String[] fruits = {"apple", "orange", "banana"};
-//        String[] vegetables = {"potato", "onion", "carrot"};
-//        String[] meats = {"chicken", "pork", "beef", "fish"};
+ /*
 
-        char[][] telephone = {{'1', '2', '3'},
-                {'4', '5', '6'},
-                {'7', '8', '8'},
-                {'*', '0', '#'}};
+ * Question array []
+ * Options array [][]
+ * Declare variables
+ * Welcome message
+  Question(loop)
+    Options
+    Get guess from user
+    check our guess
+ Display final score
 
+  */
 
-        for (char[] row : telephone) {
-            for (char number : row) {
-                System.out.print(number + " ");
-            }
-            System.out.println();
-        }
+        String[] questions = {"What is the main function of a router?",
+                "Which part of the computer is considered the brain",
+                "What year was Facebook launched?",
+                "Who is known as the father of computer?",
+                "What was the first programming language?"};
+
+        String[][] options = {
+                {"1. Storing files", "2. Encrypting data", "3. Directing internet traffic", "4.Managing passwords"},
+                {"1. CPU", "2. Hard Drive", "3. RAM", "4. GPU"},
+                {"1. 2000", "2. 2004", "3. 2006", "4. 2008"},
+                {"1. Steve Jobs", "2. Bill Gates", "3. Alan Turing", "4. Charles Babbage"},
+                {"1. COBOL", "2. C", "3. Fortran", "4. Assembly"}
+        };
+
 
     }
 
